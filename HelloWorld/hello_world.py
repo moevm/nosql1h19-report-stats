7eaf5ab0764af5ac43291914179131e7429714b4
AleksandrRybin <vfibyrf007@gmail.com>
2019-03-11 15:17:17 +0300

    new tests

--- HelloWorld/hello_world.py
+++ HelloWorld/hello_world.py
@@ -4,7 +4,7 @@ sys.path.append('../')
 from src.text_processor import TextProcessor
 from src.reports_data_base import ReportsDataBase
 from src.report import Report
-        
+
 if __name__ == "__main__":
     actions = '''
         - [0] для тестирования кода в секции для теста
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             print(f'''
                     title: [{report["title"]}] 
                     num_unique_words: [{report["words"]["total_unique_words"]}]
-                    most_popular_words: [{report["words"]["most_popular_words"]}]
+                    most_popular_words: {report["words"]["most_popular_words"][:5]}
                     ''')
 
         print('stat of group 6304:')
@@ -48,7 +48,15 @@ if __name__ == "__main__":
 
         print('stat of groups:')
         for result in db.get_stat_by_groups():
-            print(f'group: [{result["_id"]}] total_reports_loaded: [{result["total_reports_loaded"]}]')        
+            print(f'group: [{result["_id"]}] total_reports_loaded: [{result["total_reports_loaded"]}]')
+
+        print('stat of author Рыбин А.С.')
+        stat = db.get_stat_of_author('Рыбин А.С.')
+        print(f'''
+                avg_unique_words: [{stat["avg_unique_words"]}],
+                total_unique_words: [{stat["total_unique_words"]}]
+                unique_words: {stat["unique_words"][:25]}
+                ''')
     elif action == 2:
         # Очистка коллекции с отчетами
 
