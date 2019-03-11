8a8ca7a1d5148c60556112a9e912227aaec53669
AleksandrRybin <vfibyrf007@gmail.com>
2019-03-11 23:53:44 +0300

    useless index groups and course order is same

--- src/reports_data_base.py
+++ src/reports_data_base.py
@@ -15,7 +15,6 @@ class ReportsDataBase:
         
         self.db['reports'].create_index([
             ('group', pymongo.ASCENDING), 
-            ('course', pymongo.ASCENDING), 
             ('faculty', pymongo.ASCENDING), 
             ('department', pymongo.ASCENDING)
         ])
